7d14cd90bab438b2e3960fde7ab2459d0f3a7550
liton-das <litonchandra094@gmail.com>
2025-06-19 01:05:00 +0300

    work with hex generator fumction

--- js/script.js
+++ js/script.js
@@ -11,9 +11,8 @@ const change_bgBtn = document.querySelector('.change_bgBtn')
  * this function will change the display background color
  */
 change_bgBtn.addEventListener('click',()=>{
-    const color = rgbGenerator()
+    let color = twoColorGenerator()
     update_dom(color)
-    
 })
 /**
  * this function will upte the dom 
@@ -22,14 +21,37 @@ change_bgBtn.addEventListener('click',()=>{
  */
 
 const update_dom=(color)=>{
-    document.querySelector('.display_color').style = `background:${color}`
-    document.querySelector('.color_box').style = `background:${color}`
-    document.querySelector('.rgb_input').value = `${color}`
+    let rgb = rgbGenerator(color)
+    const hex = hexGenerator(color)
+    console.log(hex);
+    
+    document.querySelector('.display_color').style = `background:${rgb}`
+    document.querySelector('.color_box').style = `background:${rgb}`
+    document.querySelector('.rgb_input').value = `${rgb}`
+    document.querySelector('.hex_input').value = `${hex}`
 
 }
-
-
-
+/**
+ * this function will return color object 
+ * @returns {object}
+ */
+const twoColorGenerator = () => {
+  let red = Math.floor(Math.random() * 255);
+  let green = Math.floor(Math.random() * 255);
+  let blue = Math.floor(Math.random() * 255);
+  return {
+    red,
+    green,
+    blue
+  }
+};
+const hexGenerator=({red,green,blue})=>{
+    const rgbTohexConvert=(value)=>{
+        const hex = value.toString(16).replace(/|s+/g,'')
+        return hex.length===1?`0${hex}`:hex
+    }
+    return `#${rgbTohexConvert(red)}${rgbTohexConvert(green)}${rgbTohexConvert(blue)}`
+}
 
 
 
@@ -38,10 +60,7 @@ const update_dom=(color)=>{
  * @param {string}
  * @returns {string}
  */
-    const rgbGenerator=()=>{
-        let red = Math.floor(Math.random() * 255)
-        let green = Math.floor(Math.random() * 255)
-        let blue = Math.floor(Math.random() * 255)
+    const rgbGenerator=({red,green,blue})=>{
         return `rgb(${red},${green},${blue})`
     }
 }
