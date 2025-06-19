47426ccd8fcdda471b98ad2bacb42bf84b3672c2
liton-das <litonchandra094@gmail.com>
2025-06-19 16:46:23 +0300

    work with copy rgb  color functionality

--- js/script.js
+++ js/script.js
@@ -11,21 +11,36 @@ const hex_copy = document.querySelector('.copy_hex')
 /**
  * this function will change the display background color
  */
+    let hex = document.querySelector('.hex_input')
+    let rgb_input =document.querySelector('.rgb_input')
+     let check_icon = document.querySelector('.check_icon')
+     let copy_hex = document.querySelector('.copy_hex')
+     let copy_rgb = document.querySelector('.copy_rgb')
+     let check_iconTwo = document.querySelector('.check_iconTwo')
 change_bgBtn.addEventListener('click',()=>{
-    document.querySelector('.check_icon').style = `display:none`
-    document.querySelector('.copy_hex').style = `display : block`
+    check_icon.style = `display:none`
+    copy_hex.style = `display : block`
+    check_iconTwo.style = `display:none`
+    copy_rgb.style = `display : block`
     let color = twoColorGenerator()
     update_dom(color)
 })
+     /**
+      * this function work will copy hex color 
+      */
 hex_copy.addEventListener('click',()=>{
-     let hex = document.querySelector('.hex_input')
-     let check_icon = document.querySelector('.check_icon')
     window.navigator.clipboard.writeText(hex.value)
     check_icon.style = `display:block`
-    document.querySelector('.copy_hex').style = `display : none`
+    copy_hex.style = `display : none`
 })
-
-
+/**
+ * this function will copy rgb color only
+ */
+copy_rgb.addEventListener('click',()=>{
+    window.navigator.clipboard.writeText(rgb_input.value)
+    copy_rgb.style = 'display: none'
+    check_iconTwo.style = 'display:block'
+})
 
 // event all function end ----------
 /**
